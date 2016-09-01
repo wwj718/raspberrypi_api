3d02c380d3d4171e9b728c7dc7021c0e35f92891
wwj718 <wuwenjie718@gmail.com>
2016-09-01 16:42:01 +0800

    Update led_server.py

--- led_server.py
+++ led_server.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 CORS(app)
 # 对硬件的操作参考：http://blog.mangolovecarrot.net/2015/04/20/raspi-study01/ , 感谢 mango 同学
 # 指定GPIO口的选定模式为GPIO引脚编号模式（而非主板编号模式）
-RPi.GPIO.setmode(RPi.GPIO.BCM)
+#RPi.GPIO.setmode(RPi.GPIO.BCM)
 
 # 指定GPIO14（就是LED长针连接的GPIO针脚）的模式为输出模式
 # 如果上面GPIO口的选定模式指定为主板模式的话，这里就应该指定8号而不是14号。
-RPi.GPIO.setup(14, RPi.GPIO.OUT)
+#RPi.GPIO.setup(14, RPi.GPIO.OUT)
 
 # 循环10次
 @app.route('/led_up')
@@ -37,16 +37,16 @@ def led_up_down():
 
 @app.route('/led_up_down')
 def led_up_down():
-  for i in range(0, 5):
-    # 让GPIO14输出高电平（LED灯亮）
-    RPi.GPIO.output(14, True)
-    # 持续一段时间
-    time.sleep(0.5)
-    # 让GPIO14输出低电平（LED灯灭）
-    RPi.GPIO.output(14, False)
-    # 持续一段时间
-    time.sleep(0.5)
-  return 'ok'
+    for i in range(0, 5):
+        # 让GPIO14输出高电平（LED灯亮）
+        RPi.GPIO.output(14, True)
+        # 持续一段时间
+        time.sleep(0.5)
+        # 让GPIO14输出低电平（LED灯灭）
+        RPi.GPIO.output(14, False)
+        # 持续一段时间
+        time.sleep(0.5)
+    return 'ok'
 
 
 # 最后清理GPIO口（不做也可以，建议每次程序结束时清理一下，好习惯）
@@ -54,4 +54,4 @@ def led_up_down():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='80')
+    app.run(host='0.0.0.0',port='5000')
